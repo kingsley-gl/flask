7ea87868ae3203ffd0501398db98a2093bc43e6e
centos_desk <kingsley@localhost.localdomain>
2017-11-24 02:12:25 -0800

    build template

--- flask/flask_app/views/home.py
+++ flask/flask_app/views/home.py
@@ -14,4 +14,8 @@ home = Blueprint('home', __name__)
 
 @home.route('/',methods=['GET','POST'])
 def index():
+	return render_template('login.html')
+
+@home.route('/test',methods=['GET','POST'])
+def test():
 	return render_template('index.html')
